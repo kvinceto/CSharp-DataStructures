f13746167b039a9e61af4feb9c9cbe5e8f35c5e2
Krasimir Iliev <106111929+kvinceto@users.noreply.github.com>
2022-12-22 20:56:49 +0200

    Add files for advanced LAB 1

---
+++ Advanced/01. B-Trees-2-3-Trees-and-Red-Black-Trees-Lab-Skeleton/01. B-Trees-2-3-Trees-and-Red-Black-Trees-Lab-Skeleton/Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using _01.Two_Three;
+
+namespace Demo
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var tree = new TwoThreeTree<string>();
+
+            tree.Insert("A");
+            tree.Insert("B");
+            tree.Insert("C");
+            Console.WriteLine("B " + Environment.NewLine +
+                              "A " + Environment.NewLine +
+                              "C");
+            Console.WriteLine(tree.ToString().Trim());
+        }
+    }
+}
